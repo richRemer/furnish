89945a2c9243d60e2e643f376c1bd18f9e29d1bd
Richard Remer <richard.remer@searchoptics.com>
2014-12-26 16:47:33 -0800

    add db and dbUrl properties to Modeler instances

--- test/test-furnish.js
+++ test/test-furnish.js
@@ -2,8 +2,29 @@ var furnish = require(".."),
     Modeler = require("../lib/modeler"),
     expect = require("expect.js");
 
-describe("furnish module", function() {
+describe("<module>", function() {
     it("should export Modeler.create", function() {
         expect(furnish).to.be(Modeler.create);
+    });
+});
+
+describe("Modeler", function() {
+    var url = "http://example.com/db";
+
+    describe(".create", function() {
+        var modeler = Modeler.create(url);
+
+        it("should return Modeler instance", function() {
+            expect(modeler).to.be.a(Modeler);
+        });
+
+        it("should set dbUrl", function() {
+            expect(modeler.dbUrl).to.be(url);
+        });
+    });
+
+    it("should expose DB object", function() {
+        var modeler = Modeler.create(url);
+        expect(modeler.db).to.be.an("object");
     });
 });
